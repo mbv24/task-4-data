6cabd56264af87b0e1441f90728efee38d8c41c7
mbv24 <mbv24@cornell.edu>
2017-07-24 15:35:00 -0400

    changed questions in script

--- task-4-script.sh
+++ task-4-script.sh
@@ -2,31 +2,31 @@
 
 # Survey questions
 
-#
-echo "Question 1"
-read VAR1
-#
-echo "Question 2"
-read VAR2
-#
-echo "Question 3"
-read VAR3
-#
-echo "Question 4"
-read VAR4
-# 
-echo "Question 5"
-read VAR5
+# first question
+echo "In which month were you born?"
+read BIRTHMONTH
+# second question
+echo "How many languages can you speak?"
+read LANGNUMBER
+# third question
+echo "In which country were you born?"
+read BIRTHCOUNTRY
+# fourth question
+echo "What is your middle initial? If not applicable, write 'N/A'"
+read MIDINITIAL
+# fifth question
+echo "What language would you like to learn?"
+read LANGWANT
 # add a variable for the time
 TIMESTAMP=`date --iso-8601=seconds`
 # create unique identifier
 IDENTIFIER=`echo"$RANDOM$RANDOM$RANDOM"`
 # write data to csv file
-echo "$IDENTIFIER,$TIMESTAMP,$VAR1,$VAR2,$VAR3,$VAR4,$VAR5" >> ./data.csv
+echo "$IDENTIFIER,$TIMESTAMP,$BIRTHMONTH,$LANGNUMBER,$BIRTHCOUNTRY,$MIDINITIAL,$LANGWANT" >> ./data.csv
 # read out data in csv file 
 cat data.csv  
 # write data to database--remove later
 # bash ./write-to-db.sh
 # write to database 
-mysql -u root -p"root" -e "LOAD DATA INFILE './data.csv INTO TABLE Data FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';"
-mysqldump -u root -p"root" task4 > task4.sql
+# mysql -u root -p"root" -e "LOAD DATA INFILE './data.csv INTO TABLE Data FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';"
+# mysqldump -u root -p"root" task4 > task4.sql
