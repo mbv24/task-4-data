591f78ed143828f2cc1e5ee183d75d3b33b352c1
mbv24 <mbv24@cornell.edu>
2017-07-24 12:16:34 -0400

    changed timestamp

--- task-4-script.sh
+++ task-4-script.sh
@@ -11,5 +11,5 @@ echo "Question 4"
 read VAR4
 echo "Question 5"
 read VAR5
-TIMESTAMP='date --iso-8601=seconds'
+TIMESTAMP=`date --iso-8601=seconds`
 echo "$TIMESTAMP,$VAR1,$VAR2,$VAR3,$VAR4,$VAR5"
