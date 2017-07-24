db1b0f5f81c59c73493d340302ae6303bed7fb83
mbv24 <mbv24@cornell.edu>
2017-07-24 12:18:48 -0400

    changed script

--- task-4-script.sh
+++ task-4-script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-touch survey.csv
 echo "Question 1"
 read VAR1
 echo "Question 2"
@@ -12,4 +11,4 @@ read VAR4
 echo "Question 5"
 read VAR5
 TIMESTAMP=`date --iso-8601=seconds`
-echo "$TIMESTAMP,$VAR1,$VAR2,$VAR3,$VAR4,$VAR5"
+echo "$TIMESTAMP,$VAR1,$VAR2,$VAR3,$VAR4,$VAR5" > tmp.csv
