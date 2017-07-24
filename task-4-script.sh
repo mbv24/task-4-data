ae5c91d1fa1b89e2cbfa5243558379b86bf6f611
mbv24 <mbv24@cornell.edu>
2017-07-24 12:14:12 -0400

    changed script

--- task-4-script.sh
+++ task-4-script.sh
@@ -7,3 +7,9 @@ echo "Question 2"
 read VAR2
 echo "Question 3"
 read VAR3
+echo "Question 4"
+read VAR4
+echo "Question 5"
+read VAR5
+TIMESTAMP='date --iso-8601=seconds'
+echo "$TIMESTAMP,$VAR1,$VAR2,$VAR3,$VAR4,$VAR5"
